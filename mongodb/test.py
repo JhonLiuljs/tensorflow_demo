131a694b8bdc3e0371b970ec39518c243c5bc9f8
liujunshuai <liujunshuai@passiontec.cn>
2019-02-20 16:57:13 +0800

    init

--- mongodb/test.py
+++ mongodb/test.py
@@ -1,6 +1,11 @@
 # coding:utf-8
 from bson import json_util as jsonb
 import pymongo
-client= pymongo.MongoClient("127.0.0.1",27017)
-db=client.myinfo
-print(jsonb.dumps(list(db.user.find({"name":"wu"}))))
+
+client = pymongo.MongoClient("127.0.0.1", 27017)
+db = client.stock
+Stock = db.Stock
+results = list(Stock.find({"type": "sz", "code": "000001"}))
+for result in results:
+    print(result)
+
