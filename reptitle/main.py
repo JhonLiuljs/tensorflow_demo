303fe175c0ad53f8fbb5ccb2eb5f358b5a3a807e
liujunshuai <liujunshuai@passiontec.cn>
2019-02-20 17:44:58 +0800

    init

--- reptitle/main.py
+++ reptitle/main.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
-from scrapy import cmdline
+import sys
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
 
 if __name__ == '__main__':
-    cmdline.execute("scrapy crawl stock".split())
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('stock')
+    process.start()
+    sys.exit()
+
--- reptitle/spiders/stock_spider.py
+++ reptitle/spiders/stock_spider.py
@@ -18,12 +18,15 @@ class StockSpider(scrapy.Spider):
                 href = sel.xpath('a/@href').extract()[0]
                 try:
                     code_all = re.findall(r"[s][hz]\d{6}", href)[0]
-                    item = StockItem()
-                    item["name"] = re.sub(r'\(.*\)', "", text)
-                    item["type"] = code_all[0:2]
-                    item["code"] = code_all[2:]
-                    item["east_money_detail_url"] = href
-                    yield item
+                    type = code_all[0:2]
+                    code = code_all[2:]
+                    if code[0] == 6 or code[0] == 3 or code[0] == 0:
+                        item = StockItem()
+                        item["name"] = re.sub(r'\(.*\)', "", text)
+                        item["type"] = type
+                        item["code"] = code
+                        item["east_money_detail_url"] = href
+                        yield item
                 except Exception as e:
                     print(e)
                     continue
